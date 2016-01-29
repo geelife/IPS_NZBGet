e28389ca10cf19b1f5df502e8fc06bea26957142
geelife <geelife@gee-life.com>
2016-01-29 15:15:21 +0100

    added IO instance

---
+++ GL_NZBGetIO/module.php
@@ -0,0 +1,15 @@
+<?
+    // Klassendefinition
+    class NZBGetIO extends IPSModule {
+        /**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * ABC_MeineErsteEigeneFunktion($id);
+        *
+        */
+        public function MeineErsteEigeneFunktion() {
+            echo $this->InstanceID;
+        }
+    }
+?>
